c9f152a6f072b89d4abf72a840b6bb4512a59681
Zed <skhull.coding@proton.me>
2025-01-14 23:29:11 +0530

    update password script

---
+++ components/scripts/password.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import { User } from "../../models/User.js";
+
+const hashPasswordAndSave = async (username, password) => {
+  const hashedPassword = bcrypt.hashSync(password, 13);
+
+  const user = await User.create({
+    username,
+    password: hashedPassword,
+  });
+
+  return user ? true : false;
+};
+
+const checkPassword = async (username, password) => {
+  try {
+    const user = await User.findOne({ username });
+    if (user) {
+      let checked = bcrypt.compareSync(password, user.password);
+      return checked;
+    }
+  } catch {}
+  return false;
+};
+
+export const Password = { hashPasswordAndSave, checkPassword };
