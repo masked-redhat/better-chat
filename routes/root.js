fe4d3c97f1f501be55352e2bdc5db67e9b25bcc4
Zed <skhull.coding@proton.me>
2025-01-20 12:42:59 +0530

    using validate middleware

--- routes/root.js
+++ routes/root.js
@@ -1,20 +1,10 @@
 import { Router } from "express";
-import Auth from "../middlewares/auth.js";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-  try {
-    let cookies = req.cookies;
-    let match = await Auth.validate(cookies.usrID, cookies.__enc);
-    if (match) {
-      res.status(200).redirect("/home");
-    } else {
-      res.render("signin");
-    }
-  } catch {
-    res.render("signin");
-  }
+  if (req.user) res.status(200).redirect("/home");
+  else res.render("signin");
 });
 
 export const RootRouter = router;
