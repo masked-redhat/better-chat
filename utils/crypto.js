326750aa1efb966d46318a3287208a226ea6cb34
Zed <skhull.coding@proton.me>
2025-01-20 11:29:14 +0530

    fix deprecation function

--- utils/crypto.js
+++ utils/crypto.js
@@ -3,7 +3,7 @@
 
 const crypt = (salt, text) => {
   const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-  const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+  const byteHex = (n) => ("0" + Number(n).toString(16)).substring(-2);
   const applySaltToChar = (code) =>
     textToChars(salt).reduce((a, b) => a ^ b, code);
 
